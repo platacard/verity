726e62d250a676d3b9777751d079b6ed6028f284
marselkhisamov <marsel.khisamov@icloud.com>
2024-08-13 15:43:45 +0400

    feat(verity): release ci config

--- libs/scopes/src/lib/models/ScopeExtended.ts
+++ libs/scopes/src/lib/models/ScopeExtended.ts
@@ -2,9 +2,9 @@ import { Prisma } from '@prisma/client';
 
 export type ScopeExtended = Prisma.ScopeGetPayload<{
   include: {
-    users: true;
     apps: true;
     versions: true;
     dependencies: true;
+    users: true;
   };
 }>;
